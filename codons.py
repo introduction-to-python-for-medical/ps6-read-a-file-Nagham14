073c416e25ad5ea76347fcec0713c85fec558806
Nagham14 <naghammahajne@gmail.com>
2024-12-10 23:19:11 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,15 +1,13 @@
 def create_codon_dict(file_path):
-    
-     path="data/codons.txt"
-
-     file=open(path)
-     rows=file.readlines()
-     file.close()
-     codon_amino_dict={}
-     for row in rows:
-         cells=row.strip().split('\t') 
-         codon=cells[0]
-         amino_acid=cells[2]
-         codon_amino_dict[codon]=[amino_acid]
-     return codon_amino_dict
+    path = "data/codons.txt"
+    file = open(path)
+    rows=file.readlines()
+    file.close()
+    codon_amino_dict={}
+    for row in rows:
+        cells=row.strip().split('\t') 
+        codon=cells[0]
+        amino_acid=cells[2]
+        codon_amino_dict[codon]=[amino_acid]
+    return codon_amino_dict
 
